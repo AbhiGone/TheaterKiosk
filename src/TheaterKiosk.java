44958177d0b2a158691c49e219b0a776e9d05e4d
AbhiGone <giriabhinaya@gmail.com>
2023-09-28 09:02:30 -0400

    finished

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,5 +1,13 @@
-public class Main {
+import java.util.Scanner;
+
+public class TheaterKiosk {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("“Enter your age”");
+        int age = scan.nextInt();
+        if(age >= 21){
+            System.out.println("You get a wristband");
+        }
+
     }
 }
